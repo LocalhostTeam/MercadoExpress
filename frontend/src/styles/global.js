5ef6bfc90f64bf22a89fb76750a96ecfa48ac618
Matheus Mercadante de França <matheusmercadantedefranca@Matheuss-iMac.local>
2020-05-05 17:13:12 -0300

    Mudança na home do site (não terminado)

--- frontend/src/styles/global.js
+++ frontend/src/styles/global.js
@@ -1,11 +1,31 @@
 import { createGlobalStyle } from 'styled-components';
 
+import ArialRoundedLight from '../assets/fonts/arial-rounded/arial-rounded-mt-light.ttf';
+import ArialRounded from '../assets/fonts/arial-rounded/arial-rounded-mt.ttf';
+import ArialRoundedBold from '../assets/fonts/arial-rounded/arial-rounded-mt-bold.ttf';
 import MuseoSlab500 from '../assets/fonts/museo-slab/Museo_Slab_500.otf';
 
 export default createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
-  @import url('https://fonts.googleapis.com/css?family=Poppins:300,400,500,700&display=swap');
-  @import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
+  @font-face {
+    font-family: ArialRoundedLight;
+    src: url(${ArialRoundedLight});
+    font-weight: 300;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: ArialRounded;
+    src: url(${ArialRounded});
+    font-weight: 500;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: ArialRoundedBold;
+    src: url(${ArialRoundedBold});
+    font-weight: 700;
+    font-style: normal;
+  }
+
   @font-face {
     font-family: MuseoSlab;
     src: url(${MuseoSlab500});
@@ -23,8 +43,8 @@ export default createGlobalStyle`
     overflow-x: hidden;
   }
 
-  body, button {
-    font-family: Roboto, sans-serif;
+  body {
+    font-family: ArialRounded;
     font-size: 14px;
   }
 
@@ -47,6 +67,12 @@ export default createGlobalStyle`
     max-width: 1280px;
     margin: 0 auto;
     padding: 0 20px 50px;
+  }
+
+  .container-app {
+    width: 1440px;
+    max-width: 1440px;
+    margin-right: auto;
   }
 
   .column {
@@ -72,23 +98,38 @@ export default createGlobalStyle`
     margin-bottom: 150px;
   }
 
-  .button{
+  .button {
     width: 100%;
-    height: 50px;
+    height: 45px;
 
-    background: linear-gradient(140deg,#F873CF, #5129B2);
+    background: #16A08B;
     border: 0;
-    border-radius: 50px;
-    box-shadow: 0 3px 20px rgba(0, 0, 0, 0.15);
+    border-radius: 20px;
 
     color: #FFF;
     font-weight: 700;
     text-decoration: none;
     text-align: center;
-    font-size: 18px;
+    font-size: 15px;
+}
+
+.button2 {
+    width: 100%;
+    height: 45px;
+
+    background: none;
+    border: 1px solid #212121;
+    border-radius: 20px;
+
+    color: #111111;
+    font-weight: 700;
+    text-decoration: none;
+    text-align: center;
+    font-size: 15px;
 }
 
   button {
+    font-family: Roboto, sans-serif;
     cursor: pointer;
   }
 `;
