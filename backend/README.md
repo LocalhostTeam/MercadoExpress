a6b65ed4d938762fe09c80b15ce5dd97d907789d
lucas souza <wf_tom@hotmail.com.br>
2020-06-15 19:50:44 -0300

    Correção do relacionameto de product, brand, productBrand, market, productMarket

--- backend/README.md
+++ backend/README.md
@@ -96,3 +96,5 @@ Nest is an MIT-licensed open source project. It can grow thanks to the sponsors 
 ## License
 
   Nest is [MIT licensed](LICENSE).
+
+await queryRunner.query(`ALTER TABLE "product_brand" ADD CONSTRAINT "FK_ad86ddf61021f22143a805b62a3" UNIQUE ("brandId", "productId")`, undefined);
--- backend/src/product-brand/product-brand.entity.ts
+++ backend/src/product-brand/product-brand.entity.ts
@@ -1,14 +1,4 @@
-import {
-    Entity,
-    PrimaryColumn,
-    ManyToOne,
-    Column,
-    ManyToMany,
-    JoinTable,
-    PrimaryGeneratedColumn,
-    OneToMany,
-    JoinColumn,
-} from 'typeorm';
+import { Entity, PrimaryColumn, ManyToOne, Column, ManyToMany, JoinTable, PrimaryGeneratedColumn, OneToMany, JoinColumn} from 'typeorm';
 import { Product } from 'src/product/product.entity';
 import { Brand } from 'src/brand/brand.entity';
 import { Market } from 'src/market/market.entity';
@@ -16,35 +6,22 @@ import { Market } from 'src/market/market.entity';
 @Entity()
 export class ProductBrand {
     @PrimaryColumn({ type: 'integer' })
-    marketId!: number;
-
-    @OneToMany(
-        type => Market,
-        market => market.id,
-    )
-    @JoinColumn({ name: 'marketId' })
-    public markets!: Market[];
-
-    @PrimaryColumn({ type: 'integer' })
     brandId!: number;
 
-    @ManyToOne(
-        type => Brand,
-        brand => brand.productBrands,
-    )
+    @ManyToOne( type => Brand, brand => brand.productBrands )
     @JoinColumn({ name: 'brandId' })
     public brand!: Brand;
+
+    @PrimaryColumn({ type: 'integer' })
+    productId!: number;
+
+    @ManyToOne( type => Product, product => product.productBrands )
+    @JoinColumn({ name: 'productId' })
+    public product!: Product;
 
     @Column({ length: 255 })
     public image!: string;
 
-    @Column()
-    productId!: number;
-
-    @ManyToOne(
-        type => Product,
-        product => product.productBrands,
-    )
-    @JoinColumn({ name: 'productId' })
-    public product!: Product;
+    @OneToMany( type => Market, market => market.id )
+    public markets!: Market[];
 }
